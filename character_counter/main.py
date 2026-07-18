6ad1812d4152290f15bdb5fff3fa5db271ce9569
Mohammad Al-Hashim <mohammad.ahmad.al.hashim@gmail.com>
2026-07-18 16:56:38 +0300

    Add explanatory comment to character counter script

--- character_counter/main.py
+++ character_counter/main.py
@@ -1,2 +1,3 @@
+# This program counts the number of characters in a user-provided text.
 user_text = input("Please write your text \n")
 print("Your text contains: " + str(len(user_text)) + " characters")
