8c7e3b974603cc8416199b98b4d571624b88fe96
Mohammad Al-Hashim <mohammad.ahmad.al.hashim@gmail.com>
2026-07-20 15:39:32 +0300

    add main.py

--- land_price_calculator/main.py
+++ land_price_calculator/main.py
@@ -1,1 +1,7 @@
-
+length=float(input("Please enter the length \n"))
+width=float(input("Please enter the width \n"))
+price=float(input("How much for one meter? \n"))
+total_area=length*width
+wage=total_area*price
+print("The total area is:",total_area)
+print("Give the guy:",wage)
