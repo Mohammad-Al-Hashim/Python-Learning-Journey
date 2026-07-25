84ffd70369719fb989ea9d63ae47d63878ab2df0
Mohammad Al-Hashim <mohammad.ahmad.al.hashim@gmail.com>
2026-07-25 13:11:43 +0300

    add main.py file

---
+++ grade-evaluator/main.py
@@ -0,0 +1,9 @@
+student_grade=float(input("Please enter your grade \n"))
+if student_grade >= 90:
+    print("EXCELLENT! 🏆")
+elif student_grade >= 75:
+    print("GOOD! 🏅")
+elif student_grade >= 50:
+    print("ACCEPTAPLE 👍")
+else:
+    print("NEEDS IMPROVEMENT 📈")
