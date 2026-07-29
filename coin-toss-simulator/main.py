ce79d863e5e59b93864b5ae92a66335ce5fc591c
Mohammad Al-Hashim <mohammad.ahmad.al.hashim@gmail.com>
2026-07-29 22:42:17 +0300

    Add code to main.py

--- coin-toss-simulator/main.py
+++ coin-toss-simulator/main.py
@@ -1,1 +1,14 @@
-
+import random
+print("""
+█░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   ▀█▀ █▀█   ▀█▀ █░█ █▀▀   █░█ █ █▀█ ▀█▀ █░█ ▄▀█ █░░   █▀▀ █▀█ █ █▄░█   ▀█▀ █▀█ █▀ █▀
+▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   ░█░ █▄█   ░█░ █▀█ ██▄   ▀▄▀ █ █▀▄ ░█░ █▄█ █▀█ █▄▄   █▄▄ █▄█ █ █░▀█   ░█░ █▄█ ▄█ ▄█  
+       
+█▀▀ ▄▀█ █▀▄▀█ █▀▀
+█▄█ █▀█ █░▀░█ ██▄ 
+""")
+input("Press ENTER to start")
+random_number = random.randint(0,1)
+if random_number == 0:
+    print("The result is: HEADS!")
+else:
+    print("The result is: TAILS!")
