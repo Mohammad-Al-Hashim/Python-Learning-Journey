5f249ba2e1d3ed7e3c484c99c9c99bcd348c045f
Mohammad Al-Hashim <mohammad.ahmad.al.hashim@gmail.com>
2026-07-30 23:01:55 +0300

    add code to main.py

--- movie-ticket-pricing-system/main.py
+++ movie-ticket-pricing-system/main.py
@@ -1,1 +1,13 @@
-
+age=int(input("How old are you? \n"))
+if age >= 18:
+    status=input("Type wether you are a student or employee or unemployed \n").lower()
+    if status == "student":
+        print("Your ticket price is: 15$")
+    elif status == "unemployed":
+        print("Your ticket price is 10$")
+    elif status == "employee":
+        print("Your ticket price is 20$")
+    else:
+        print("You must choose (student) or (employee) or (unemployed)")
+else:
+    print("Your ticket price is: 5$")
