b3b4f4a6543b1e32f3b6eefd1d3d68c749970dbe
Mohammad Al-Hashim <mohammad.ahmad.al.hashim@gmail.com>
2026-08-02 15:17:49 +0300

    edit main.py

--- tip-calculator/main.py
+++ tip-calculator/main.py
@@ -1,1 +1,15 @@
-
+print("Welcome to tip calculator")
+price = float(input("Please type the price \n"))
+recommended_tip_poor = price*(10/100)
+recommended_tip_good = price*(15/100)
+recommended_tip_perfect = price*(20/100)
+service = input("Please estimate the service, type: (POOR) OR (GOOD) OR (PERFECT) \n").upper()
+if service == "POOR" or service == "GOOD" or service == "PERFECT":
+    if service == "POOR":
+        print(f"The recommended tip is: {recommended_tip_poor}")
+    elif service == "GOOD":
+        print(f"The recommended tip is: {recommended_tip_good}")
+    else:
+        print(f"The recommended tip is: {recommended_tip_perfect}")
+else:
+    print("You must type: (POOR) OR (GOOD) OR (PERFECT), try again")
